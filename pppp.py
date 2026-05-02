85b83289600780db468eeaf7a30142bac9de758a
penteberidze1979-code <penteberidze1979@gmail.com>
2026-05-02 18:13:48 +0400

    Update pppp.py

--- pppp.py
+++ pppp.py
@@ -53,7 +53,7 @@ HTML_TEMPLATE = """
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TITAN OMEGA X-1 | SUPREME DASHBOARD</title>
+    <title>pentv X-1 | SUPREME DASHBOARD</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <style>
         :root {
